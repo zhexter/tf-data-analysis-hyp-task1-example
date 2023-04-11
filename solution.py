200d026042f492feb4873621f8c809a74fb68304
Eugenia <zhexter@gmail.com>
2023-04-11 10:10:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,6 +12,6 @@ def solution(x_success: int,
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 0.02
-    p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[1]
+    p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')[1]
     
     return p_value < alpha # Ваш ответ, True или False
